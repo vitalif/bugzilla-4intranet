264f6c71caf0b47985da876f6489427d558ac110
vfilippov <vfilippov@6955db30-a419-402b-8a0d-67ecbb4d7f56>
2008-12-29 17:28:44 +0000

    Bug 40933 fixes + user menu modification in flag requests (Bug 10837)
    git-svn-id: svn://svn.office.custis.ru/3rdparty/bugzilla.org/trunk@80 6955db30-a419-402b-8a0d-67ecbb4d7f56

--- Bugzilla/Config/Integration.pm
+++ Bugzilla/Config/Integration.pm
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 # Конфигурация интеграции со всевозможными Виками и Вьювисями
 
-package extensions::custis::lib::Config;
+package Bugzilla::Config::Integration;
 
 use strict;
 use warnings;
@@ -13,37 +13,37 @@ sub get_param_list
     return (
     {
         name => 'wiki_url',
-        type => 's',
+        type => 't',
         default => 'http://wiki.office.custis.ru/wiki/index.php/',
     },
 
     {
         name => 'viewvc_url',
-        type => 's',
+        type => 't',
         default => 'http://viewvc.office.custis.ru/viewvc.py/',
     },
 
     {
         name => 'smwiki_url',
-        type => 's',
+        type => 't',
         default => 'http://penguin.office.custis.ru/smwiki/index.php/',
     },
 
     {
         name => 'smboa_url',
-        type => 's',
+        type => 't',
         default => 'http://penguin.office.custis.ru/smboa/index.php/',
     },
 
     {
         name => 'sbwiki_url',
-        type => 's',
+        type => 't',
         default => 'http://sobin.office.custis.ru/sbwiki/index.php/',
     },
 
     {
         name => 'rdwiki_url',
-        type => 's',
+        type => 't',
         default => 'http://radey.office.custis.ru/rdwiki/index.php/',
     },
     );
