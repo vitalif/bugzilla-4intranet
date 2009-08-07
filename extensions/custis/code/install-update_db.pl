ca438733e4587942a2b5f38c1c93505a99d85a82
vfilippov <vfilippov@6955db30-a419-402b-8a0d-67ecbb4d7f56>
2009-08-07 08:54:25 +0000

    Bug 40933 Recode saved queries from CP1251 to UTF8
    git-svn-id: svn://svn.office.custis.ru/3rdparty/bugzilla.org/trunk@236 6955db30-a419-402b-8a0d-67ecbb4d7f56

---
+++ extensions/custis/code/install-update_db.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+# Перекодировка параметров сохранённых поисков из CP-1251 в UTF-8
+
+use strict;
+use utf8;
+use Encode;
+use URI::Escape;
+
+sub sure_utf8
+{
+    my ($s) = @_;
+    $s = uri_unescape($s);
+    Encode::_utf8_on($s);
+    my $v = utf8::valid($s);
+    Encode::_utf8_off($s);
+    Encode::from_to($s, 'cp1251', 'utf8') unless $v;
+    $s = uri_escape($s);
+    return $s;
+}
+
+print "Making sure saved queries are in UTF-8...\n";
+my $dbh = Bugzilla->dbh;
+my $nq = $dbh->selectall_arrayref("SELECT * FROM namedqueries WHERE query LIKE '%\\%%'", {Slice=>{}});
+if ($nq)
+{
+    my $q;
+    foreach (@$nq)
+    {
+        $q = $_->{query};
+        $q =~ s/(\%[0-9A-F]{2})+/sure_utf8($&)/iegso;
+        $dbh->do("UPDATE namedqueries SET query=? WHERE id=?", $q, $_->{id}) if $q ne $_->{query};
+    }
+}
