fb4c95ad44c625a42f9e339692d6fbc81f071be2
vfilippov <vfilippov@6955db30-a419-402b-8a0d-67ecbb4d7f56>
2009-08-19 16:16:31 +0000

    Bug 40933 cf_agreement based on new 3.4 restrictible custom field mechanism
    git-svn-id: svn://svn.office.custis.ru/3rdparty/bugzilla.org/trunk@263 6955db30-a419-402b-8a0d-67ecbb4d7f56

--- extensions/bugzilla-2-to-3-db-fix.sql
+++ extensions/bugzilla-2-to-3-db-fix.sql
@@ -15,11 +15,32 @@ delete from category_group_map where group_id in (17, 45, 30, 23, 21, 14, 15, 27
 -- Убираем старые юзерские настройки
 delete from profile_setting where setting_name in ('go_to_next_bug', 'remind_me_about_worktime_newbug', 'remind_me_about_requests', 'create_bug_resolved');
 -- Наполняем значениями поле "Договор" (должно быть уже создано = cf_agreement)
-insert into cf_agreement (value, sortkey, isactive)
-    select name, sortkey, 1
-    from `agreements`
-    where name not in ('!Без договора', '--', '---', '--- пусто')
-    group by name
-    order by name;
+insert into cf_agreement (value, sortkey, isactive, visibility_value_id)
+    select name, sortkey, act, (case when count(`name`) > 1 then NULL else `vis` end)
+    from (
+        select
+            (case when instr(name,' (') > 0 then substr(`name`,1,instr(name,' (')-1) else `name` end) as `name`,
+            sortkey,
+            1 as act,
+            product_id as vis
+        from agreements
+        where name not in ('!Без договора', '--', '---', '--- пусто')
+    ) as `t0`
+    group by (case when instr(name,' (') > 0 then substr(`name`,1,instr(name,' (')-1) else `name` end);
+-- Устанавливаем правильные sortkey, такие, чтобы ограниченные значения были вверху,
+create temporary table tmp1 (id int not null auto_increment primary key, sortkey int not null default 0);
+insert into tmp1 (sortkey) select distinct sortkey from cf_agreement where visibility_value_id IS NOT NULL order by sortkey;
+update cf_agreement set sortkey=5+5*(select tmp1.id from tmp1 where tmp1.sortkey=cf_agreement.sortkey) where visibility_value_id IS NOT NULL;
+drop table tmp1;
+-- А неограниченные - внизу
+create temporary table tmp1 (id int not null auto_increment primary key, sortkey int not null default 0);
+insert into tmp1 (sortkey) select distinct sortkey from cf_agreement where visibility_value_id IS NULL order by sortkey;
+update cf_agreement set sortkey=200+5*(select tmp1.id from tmp1 where tmp1.sortkey=cf_agreement.sortkey) where visibility_value_id IS NULL;
+drop table tmp1;
+-- А --- в самом верху
+update cf_agreement set sortkey=0 where value='---';
 -- Записываем значения договора для всех багов
-update bugs, agreements, cf_agreement set bugs.cf_agreement=agreements.name where bugs.agreement_id=agreements.id and agreements.name not in ('!Без договора', '--', '---', '--- пусто');
+update bugs, agreements, cf_agreement
+set bugs.cf_agreement=(case when instr(agreements.name,' (') > 0 then substr(agreements.`name`,1,instr(agreements.name,' (')-1) else agreements.`name` end)
+where bugs.agreement_id=agreements.id
+and agreements.name not in ('!Без договора', '--', '---', '--- пусто');
