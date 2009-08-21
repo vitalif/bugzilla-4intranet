f65d2df22ed98a6307e09af906d37354f6005727
vfilippov <vfilippov@6955db30-a419-402b-8a0d-67ecbb4d7f56>
2009-08-21 15:51:56 +0000

    UTF8
    git-svn-id: svn://svn.office.custis.ru/3rdparty/bugzilla.org/trunk@277 6955db30-a419-402b-8a0d-67ecbb4d7f56

--- extensions/bugzilla-2-to-3-db-fix.sql
+++ extensions/bugzilla-2-to-3-db-fix.sql
@@ -1,21 +1,21 @@
--- SQL скрипт изменений требуемых в нашей боевой базе багзиллы при обновлении с 2.x до 3.х
--- Возможно, добавятся ещё новые кривости, но пока так.
+-- SQL я│п╨я─п╦п©я┌ п╦п╥п╪п╣п╫п╣п╫п╦п╧ я┌я─п╣п╠я┐п╣п╪я▀я┘ п╡ п╫п╟я┬п╣п╧ п╠п╬п╣п╡п╬п╧ п╠п╟п╥п╣ п╠п╟пЁп╥п╦п╩п╩я▀ п©я─п╦ п╬п╠п╫п╬п╡п╩п╣п╫п╦п╦ я│ 2.x п╢п╬ 3.я┘
+-- п▓п╬п╥п╪п╬п╤п╫п╬, п╢п╬п╠п╟п╡я▐я┌я│я▐ п╣я┴я▒ п╫п╬п╡я▀п╣ п╨я─п╦п╡п╬я│я┌п╦, п╫п╬ п©п╬п╨п╟ я┌п╟п╨.
 set names utf8;
--- ДО ./checksetup.pl:
+-- п■п· ./checksetup.pl:
 alter table components change wiki_url wiki_url1 varchar(255) not null;
--- ПОСЛЕ ./checksetup.pl:
+-- п÷п·п║п⌡п∙ ./checksetup.pl:
 alter table components drop wiki_url;
 alter table components change wiki_url1 wiki_url varchar(255) not null;
--- Убираем кривые значения из базы
+-- пёп╠п╦я─п╟п╣п╪ п╨я─п╦п╡я▀п╣ п╥п╫п╟я┤п╣п╫п╦я▐ п╦п╥ п╠п╟п╥я▀
 delete from profiles_activity where userid=83;
 delete from bugs_activity where attach_id=13529;
 delete from bugs_activity where attach_id=13950;
 delete from series_data where series_id in (647, 648, 649, 650, 651, 652, 681, 694, 695, 696, 697);
 delete from category_group_map where group_id in (17, 45, 30, 23, 21, 14, 15, 27, 49, 16, 10, 18, 19, 22, 35, 31);
--- Убираем старые юзерские настройки
+-- пёп╠п╦я─п╟п╣п╪ я│я┌п╟я─я▀п╣ я▌п╥п╣я─я│п╨п╦п╣ п╫п╟я│я┌я─п╬п╧п╨п╦
 delete from profile_setting where setting_name in ('go_to_next_bug', 'remind_me_about_worktime_newbug', 'remind_me_about_requests', 'create_bug_resolved');
--- Дальше ДОЛЖНО уже быть создано поле cf_agreement (договор) зависимое от продукта
--- Наполняем значениями поле "Договор"
+-- п■п╟п╩я▄я┬п╣ п■п·п⌡п√п²п· я┐п╤п╣ п╠я▀я┌я▄ я│п╬п╥п╢п╟п╫п╬ п©п╬п╩п╣ cf_agreement (п╢п╬пЁп╬п╡п╬я─) п╥п╟п╡п╦я│п╦п╪п╬п╣ п╬я┌ п©я─п╬п╢я┐п╨я┌п╟
+-- п²п╟п©п╬п╩п╫я▐п╣п╪ п╥п╫п╟я┤п╣п╫п╦я▐п╪п╦ п©п╬п╩п╣ "п■п╬пЁп╬п╡п╬я─"
 insert into cf_agreement (value, sortkey, isactive, visibility_value_id)
     select name, sortkey, act, (case when count(`name`) > 1 then NULL else `vis` end)
     from (
@@ -25,26 +25,26 @@ insert into cf_agreement (value, sortkey, isactive, visibility_value_id)
             1 as act,
             product_id as vis
         from agreements
-        where name not in ('!Без договора', '--', '---', '--- пусто', 'Unspec')
+        where name not in ('!п▒п╣п╥ п╢п╬пЁп╬п╡п╬я─п╟', '--', '---', '--- п©я┐я│я┌п╬', 'Unspec')
     ) as `t0`
     group by (case when instr(name,' (') > 0 then substr(`name`,1,instr(name,' (')-1) else `name` end);
--- Устанавливаем правильные sortkey, такие, чтобы ограниченные значения были вверху,
+-- пёя│я┌п╟п╫п╟п╡п╩п╦п╡п╟п╣п╪ п©я─п╟п╡п╦п╩я▄п╫я▀п╣ sortkey, я┌п╟п╨п╦п╣, я┤я┌п╬п╠я▀ п╬пЁя─п╟п╫п╦я┤п╣п╫п╫я▀п╣ п╥п╫п╟я┤п╣п╫п╦я▐ п╠я▀п╩п╦ п╡п╡п╣я─я┘я┐,
 create temporary table tmp1 (id int not null auto_increment primary key, sortkey int not null default 0);
 insert into tmp1 (sortkey) select distinct sortkey from cf_agreement where visibility_value_id IS NOT NULL order by sortkey;
 update cf_agreement set sortkey=5+5*(select tmp1.id from tmp1 where tmp1.sortkey=cf_agreement.sortkey) where visibility_value_id IS NOT NULL;
 drop table tmp1;
--- А неограниченные - внизу
+-- п░ п╫п╣п╬пЁя─п╟п╫п╦я┤п╣п╫п╫я▀п╣ - п╡п╫п╦п╥я┐
 create temporary table tmp1 (id int not null auto_increment primary key, sortkey int not null default 0);
 insert into tmp1 (sortkey) select distinct sortkey from cf_agreement where visibility_value_id IS NULL order by sortkey;
 update cf_agreement set sortkey=200+5*(select tmp1.id from tmp1 where tmp1.sortkey=cf_agreement.sortkey) where visibility_value_id IS NULL;
 drop table tmp1;
--- А --- в самом верху
+-- п░ --- п╡ я│п╟п╪п╬п╪ п╡п╣я─я┘я┐
 update cf_agreement set sortkey=0 where value='---';
--- Записываем значения договора для всех багов
+-- п≈п╟п©п╦я│я▀п╡п╟п╣п╪ п╥п╫п╟я┤п╣п╫п╦я▐ п╢п╬пЁп╬п╡п╬я─п╟ п╢п╩я▐ п╡я│п╣я┘ п╠п╟пЁп╬п╡
 update bugs, agreements, cf_agreement
 set bugs.cf_agreement=(case when instr(agreements.name,' (') > 0 then substr(agreements.`name`,1,instr(agreements.name,' (')-1) else agreements.`name` end)
 where bugs.agreement_id=agreements.id
-and agreements.name not in ('!Без договора', '--', '---', '--- пусто', 'Unspec');
--- Удаляем старое поле "agreement" из fielddefs
+and agreements.name not in ('!п▒п╣п╥ п╢п╬пЁп╬п╡п╬я─п╟', '--', '---', '--- п©я┐я│я┌п╬', 'Unspec');
+-- пёп╢п╟п╩я▐п╣п╪ я│я┌п╟я─п╬п╣ п©п╬п╩п╣ "agreement" п╦п╥ fielddefs
 update bugs_activity set fieldid=(select fielddefs.id from fielddefs where fielddefs.name='cf_agreement') where fieldid=(select fielddefs.id from fielddefs where fielddefs.name='agreement');
 delete from fielddefs where name='agreement';
