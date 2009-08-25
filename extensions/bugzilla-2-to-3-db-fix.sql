56c15dc32abf7d8207cc4b6dcfa30ff345d3cc54
vfilippov <vfilippov@6955db30-a419-402b-8a0d-67ecbb4d7f56>
2009-08-25 17:40:41 +0000

    Bug 40933 Setting migration
    git-svn-id: svn://svn.office.custis.ru/3rdparty/bugzilla.org/trunk@293 6955db30-a419-402b-8a0d-67ecbb4d7f56

--- extensions/bugzilla-2-to-3-db-fix.sql
+++ extensions/bugzilla-2-to-3-db-fix.sql
@@ -13,8 +13,11 @@ delete from bugs_activity where attach_id=13950;
 delete from series_data where series_id in (647, 648, 649, 650, 651, 652, 681, 694, 695, 696, 697);
 delete from category_group_map where group_id in (17, 45, 30, 23, 21, 14, 15, 27, 49, 16, 10, 18, 19, 22, 35, 31);
 -- Убираем старые юзерские настройки
+create temporary table tmp2 as select user_id, 'post_bug_submit_action' AS setting_name, (case when go_to_next_bug='on' then 'next_bug' else 'same_bug' end) as setting_value from profile_setting where setting_name='go_to_next_bug';
+replace into profile_setting (user_id, setting_name, setting_value) select * from tmp2;
+drop table tmp2;
 delete from profile_setting where setting_name in ('go_to_next_bug', 'remind_me_about_flags', 'create_bug_resolved');
-update profile_settings set setting_name='remind_me_about_flags' where setting_name='remind_me_about_requests';
+update profile_setting set setting_name='remind_me_about_flags' where setting_name='remind_me_about_requests';
 delete from setting_value where name in ('go_to_next_bug', 'remind_me_about_requests', 'create_bug_resolved');
 delete from setting where name in ('go_to_next_bug', 'remind_me_about_requests', 'create_bug_resolved');
 -- Дальше ДОЛЖНО уже быть создано поле cf_agreement (договор) зависимое от продукта
