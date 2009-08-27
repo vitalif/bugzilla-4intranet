01d093c7a1d531f0120e04802e90e02cf886924a
vfilippov <vfilippov@6955db30-a419-402b-8a0d-67ecbb4d7f56>
2009-08-27 14:55:25 +0000

    Bug 40933 Email_in aliases for users
    git-svn-id: svn://svn.office.custis.ru/3rdparty/bugzilla.org/trunk@317 6955db30-a419-402b-8a0d-67ecbb4d7f56

--- extensions/custis/code/db_schema-abstract_schema.pl
+++ extensions/custis/code/db_schema-abstract_schema.pl
@@ -16,3 +16,19 @@ $schema->{emailin_fields} = {
         emailin_fields_primary => { FIELDS => ['address', 'field'], TYPE => 'UNIQUE' },
     ],
 };
+
+# ALIASES FOR INCOMING EMAILS
+# --------------
+
+$schema->{emailin_aliases} = {
+    FIELDS => [
+        address  => {TYPE => 'varchar(255)', NOTNULL => 1},
+        userid   => {TYPE => 'INT3', NOTNULL => 1,
+                     REFERENCES => {TABLE => 'profiles',
+                                    COLUMN => 'userid'}},
+        fromldap => {TYPE => 'BOOLEAN'},
+    ],
+    INDEXES => [
+        emailin_aliases_address => { FIELDS => ['address'], TYPE => 'UNIQUE' },
+    ],
+};
