13d659997a345d0fe75c82e044d946afe59b0d26
vfilippov <vfilippov@6955db30-a419-402b-8a0d-67ecbb4d7f56>
2009-10-14 14:31:54 +0000

    Bug 52963
    git-svn-id: svn://svn.office.custis.ru/3rdparty/bugzilla.org/trunk@463 6955db30-a419-402b-8a0d-67ecbb4d7f56

--- extensions/custis/code/quote_urls-custom_proto.pl
+++ extensions/custis/code/quote_urls-custom_proto.pl
@@ -34,7 +34,7 @@ sub processWikiUrl
     return Bugzilla->params->{"${wiki}_url"} . $url . '#' . processWikiAnchor($anchor);
 }
 
-for my $wiki (qw/wiki smwiki smboa sbwiki fawiki kswiki rdwiki gzwiki dpwiki hrwiki cbwiki/)
+for my $wiki (qw/wiki smwiki smboa sbwiki fawiki kswiki rdwiki gzwiki dpwiki hrwiki cbwiki gzstable/)
 {
     Bugzilla->hook_args->{custom_proto}->{$wiki} = sub { processWikiUrl($wiki, @_) }
 }
