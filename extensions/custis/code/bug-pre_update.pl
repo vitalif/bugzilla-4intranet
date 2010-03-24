037744650f92467b4491397b7fe052ccda283b69
vfilippov <vfilippov@6955db30-a419-402b-8a0d-67ecbb4d7f56>
2010-03-24 17:54:37 +0000

    Bug 61996 - Only croak when CHANGING to CLOSED
    git-svn-id: svn://svn.office.custis.ru/3rdparty/bugzilla.org/trunk@688 6955db30-a419-402b-8a0d-67ecbb4d7f56

--- extensions/custis/code/bug-pre_update.pl
+++ extensions/custis/code/bug-pre_update.pl
@@ -5,8 +5,10 @@ use Bugzilla::Util qw(trim);
 use Bugzilla::Error;
 
 my $bug = Bugzilla->hook_args->{bug};
+my $old = Bugzilla::Bug->new({ id => $bug->id });
 my $nok = {};
-if ($bug->product eq 'TN-RMS' && $bug->{bug_status} eq 'CLOSED')
+if ($bug->product eq 'TN-RMS' && $old->{bug_status} ne 'CLOSED' &&
+    $bug->{bug_status} eq 'CLOSED')
 {
     $nok->{s} = 1 if $bug->{cf_sprint} =~ /^\s*$/so;
     $nok->{w} = 1 if $bug->{status_whiteboard} =~ /^\s*$/so;
