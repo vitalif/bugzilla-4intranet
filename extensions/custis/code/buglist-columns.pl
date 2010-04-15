6e2aee33cdc64369a0c4d1bc04b05487d71d9521
vfilippov <vfilippov@6955db30-a419-402b-8a0d-67ecbb4d7f56>
2010-04-15 14:07:07 +0000

    Bug 62964 - Add Flags and Requests columns
    git-svn-id: svn://svn.office.custis.ru/3rdparty/bugzilla.org/trunk@707 6955db30-a419-402b-8a0d-67ecbb4d7f56

--- extensions/custis/code/buglist-columns.pl
+++ extensions/custis/code/buglist-columns.pl
@@ -13,3 +13,20 @@ $columns->{blocked} = {
     name  => "(SELECT GROUP_CONCAT(bugblocked.blocked SEPARATOR ',') FROM dependencies bugblocked WHERE bugblocked.dependson=bugs.bug_id)",
     title => "Bugs blocked",
 };
+
+$columns->{flags} = {
+    name  =>
+"(SELECT GROUP_CONCAT(CONCAT(col_ft.name,col_f.status) SEPARATOR ', ')
+FROM flags col_f JOIN flagtypes col_ft ON col_f.type_id=col_ft.id
+WHERE col_f.bug_id=bugs.bug_id AND col_ft.is_requesteeble=0)",
+    title => "Flags",
+};
+
+$columns->{requests} = {
+    name  =>
+"(SELECT GROUP_CONCAT(CONCAT(col_ft.name,col_f.status,CASE WHEN col_p.login_name IS NULL THEN '' ELSE CONCAT(' ',col_p.login_name) END) SEPARATOR ', ')
+FROM flags col_f JOIN flagtypes col_ft ON col_f.type_id=col_ft.id
+LEFT JOIN profiles col_p ON col_f.requestee_id=col_p.userid
+WHERE col_f.bug_id=bugs.bug_id AND col_ft.is_requesteeble=1)",
+    title => "Requests",
+};
--- extensions/custis/code/colchange-columns.pl
+++ extensions/custis/code/colchange-columns.pl
@@ -5,3 +5,5 @@ use strict;
 my $columns = Bugzilla->hook_args->{columns};
 
 push @$columns, 'dependson', 'blocked';
+
+push @$columns, 'flags', 'requests';
