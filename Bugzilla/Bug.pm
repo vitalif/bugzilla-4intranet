687f9d6a42623b41df9caab6005790114b42ba10
vfilippov <vfilippov@6955db30-a419-402b-8a0d-67ecbb4d7f56>
2010-12-24 16:57:44 +0000

    Bug 61225
    git-svn-id: svn://svn.office.custis.ru/3rdparty/bugzilla.org/trunk@1187 6955db30-a419-402b-8a0d-67ecbb4d7f56

--- Bugzilla/Bug.pm
+++ Bugzilla/Bug.pm
@@ -1002,7 +1002,7 @@ sub update
         my @values  = values %$comment;
         my $qmarks  = join(',', ('?') x @values);
         $dbh->do("INSERT INTO longdescs ($columns) VALUES ($qmarks)", undef, @values);
-        if ($comment->{work_time})
+        if ($comment->{work_time} > 0)
         {
             # log worktime
             $changes->{work_time} ||= [ '', 0 ];
