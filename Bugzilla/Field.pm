7017896252bed9ac4ef04184bdfacb30632aedf7
vfilippov <vfilippov@6955db30-a419-402b-8a0d-67ecbb4d7f56>
2011-10-24 12:17:30 +0000

    Bug 88525 - Fix for "static" values (---)
    git-svn-id: svn://svn.office.custis.ru/3rdparty/bugzilla.org/trunk@1441 6955db30-a419-402b-8a0d-67ecbb4d7f56

--- Bugzilla/Field.pm
+++ Bugzilla/Field.pm
@@ -518,7 +518,7 @@ sub restricted_legal_values
         my $hash = Bugzilla->fieldvaluecontrol_hash->{$self->value_field_id}->{values}->{$self->id};
         $self->{restricted_legal_values}->{$controller_value} = [
             grep {
-                $_->name eq '---' || !exists $hash->{$_->id} ||
+                $_->is_static || !exists $hash->{$_->id} ||
                 !%{$hash->{$_->id}} || $hash->{$_->id}->{$controller_value}
             } @{$self->legal_values}
         ];
