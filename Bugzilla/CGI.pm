2135fe586d7f67a50dd24b8c4165d95dfa7e5607
vfilippov <vfilippov@6955db30-a419-402b-8a0d-67ecbb4d7f56>
2012-02-29 13:42:11 +0000

    Bug 75890 - Update for JSON::RPC 1.1
    git-svn-id: svn://svn.office.custis.ru/3rdparty/bugzilla.org/trunk@1519 6955db30-a419-402b-8a0d-67ecbb4d7f56

--- Bugzilla/CGI.pm
+++ Bugzilla/CGI.pm
@@ -532,7 +532,7 @@ sub cookie
                 for (keys %{$self->{'.cookies'}})
                 {
                     $v = $self->{'.cookies'}->{$_}->value;
-                    Encode::_utf8_on($_) if ?[\x80-\xFFFF]?;
+                    Encode::_utf8_on($_) if /[\x80-\xFFFF]/;
                     Encode::_utf8_on($v);
                     push @a, $_, $v;
                 }
