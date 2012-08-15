2cfcb4d9bcd543566ce50a62779813599bef9745
vfilippov <vfilippov@6955db30-a419-402b-8a0d-67ecbb4d7f56>
2012-08-15 12:13:04 +0000

    Bug 88873 - better error reporting
    git-svn-id: svn://svn.office.custis.ru/3rdparty/bugzilla.org/trunk@1598 6955db30-a419-402b-8a0d-67ecbb4d7f56

--- extensions/custishacks/lib/Bugzilla/SmClient.pm
+++ extensions/custishacks/lib/Bugzilla/SmClient.pm
@@ -19,6 +19,8 @@ use strict;
 use Bugzilla::Util;
 use Bugzilla::Error;
 use SOAP::Lite;
+use Carp;
+use Data::Dumper;
 
 # Construct an SM/dotProject WS client
 sub new
@@ -26,20 +28,18 @@ sub new
     my $class = shift;
     $class = ref($class) || $class;
     my $cli = SOAP::Lite->service(Bugzilla->params->{sm_dotproject_wsdl_url});
-    my $r = $cli->OpenSession(
+    my $self = bless { client => $cli }, $class;
+    my $r = $self->call(1, 'OpenSession',
         Bugzilla->params->{sm_dotproject_login},
         Bugzilla->params->{sm_dotproject_password},
     );
-    check_ws_error($r);
     my $data = xml_simple($r->{Data});
     if (!$data->{eSessionID})
     {
         die "No <SessionID> in SM WS answer data: $r->{Data}";
     }
-    return bless {
-        sid => $data->{eSessionID}->[0]->{char},
-        client => $cli,
-    }, $class;
+    $self->{sid} = $data->{eSessionID}->[0]->{char};
+    return $self;
 }
 
 # Destructor, closes the session
@@ -52,25 +52,44 @@ sub DESTROY
 # Check for SM WS error and throw 'sm_ws_error' if there is one
 sub check_ws_error
 {
-    my ($r) = @_;
+    my ($self, $r, $skipFrames) = @_;
     if ($r->{Status}->{ErrorCode})
     {
-        die "SM WS returned error #$r->{Status}->{ErrorCode}: $r->{Status}->{Message}";
+        local $Carp::CarpLevel = $skipFrames || 1;
+        local $Data::Dumper::Indent = 0;
+        Carp::confess(
+            "SM WS returned error #$r->{Status}->{ErrorCode}: $r->{Status}->{Message}\n".
+            "(in response to $self->{lastFn}(".substr(Dumper($self->{lastParams}), 8, -1)."))\n"
+        );
     }
+}
+
+# Wrapper for $self->{client}->call
+sub call
+{
+    my ($self, $check_err, $fn, @params) = @_;
+    $self->{lastFn} = $fn;
+    $self->{lastParams} = \@params;
+    my $r = $self->{client}->call($fn, @params);
+    if ($check_err)
+    {
+        $self->check_ws_error($r, 2);
+    }
+    return $r;
 }
 
 # Read task with all attributes
 sub read_task
 {
     my ($self, $taskUID) = @_;
-    my $r = $self->{client}->call('ReadTask', $self->{sid}, $taskUID, [])->result;
+    my $r = $self->call(0, 'ReadTask', $self->{sid}, $taskUID, [])->result;
     if ($r->{Status}->{ErrorCode} &&
         $r->{Status}->{Message} =~ /Неверный идентификатор задачи/s)
     {
         # The task does not exist
         return undef;
     }
-    check_ws_error($r);
+    $self->check_ws_error($r);
     my $data = xml_simple($r->{Data});
     my $task = {};
     foreach (@{$data->{eFieldList}->[0]->{eField}})
@@ -110,7 +129,7 @@ sub create_or_update
 {
     my ($self, $params) = @_;
     $params->{ZISTaskClass} = 'class_3';
-    my $fieldNames = qw(Name Description State Release ZISTaskClass);
+    my $fieldNames = [ qw(Name Description State Release ZISTaskClass) ];
     my $req = {
         SessionID    => $self->{sid},
         TaskUID      => $params->{TaskCUID},
@@ -141,28 +160,25 @@ sub create_or_update
         ));
         $req->{ProjectUID} = $taskB->{ProjectUID};
         # Create task
-        $r = $self->{client}->call('CreateTask',
+        $r = $self->call(1, 'CreateTask',
             @$req{qw(SessionID ProjectUID TaskUID ParentUID ComponentUID FieldList)}
         )->result;
-        check_ws_error($r);
     }
     else
     {
         # Check if we need to change any updatable fields
         if (grep { $params->{$_} ne $taskC->{$_} } @$fieldNames)
         {
-            $r = $self->{client}->call('UpdateTask',
+            $r = $self->call(1, 'UpdateTask',
                 @$req{qw(SessionID TaskUID ComponentUID FieldList)}
             )->result;
-            check_ws_error($r);
         }
         # Check if we need to change ParentUID via a separate call to ChangeTaskB
         if ($params->{TaskBUID} ne $taskC->{ParentUID})
         {
-            $r = $self->{client}->call('ChangeTaskB',
+            $r = $self->call(1, 'ChangeTaskB',
                 @$req{qw(SessionID TaskUID ParentUID)}
             )->result;
-            check_ws_error($r);
         }
     }
 }
