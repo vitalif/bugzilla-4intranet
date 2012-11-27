27c6feb31cff5a49129d2f41662079b48af747d8
vfilippov <vfilippov@6955db30-a419-402b-8a0d-67ecbb4d7f56>
2012-11-27 16:34:28 +0000

    Bug 88873 - Delete value then mapping
    git-svn-id: svn://svn.office.custis.ru/3rdparty/bugzilla.org/trunk@1629 6955db30-a419-402b-8a0d-67ecbb4d7f56

--- extensions/custishacks/lib/Bugzilla/WebService/WBS.pm
+++ extensions/custishacks/lib/Bugzilla/WebService/WBS.pm
@@ -84,8 +84,8 @@ sub delete_value
     my ($self, $params) = @_;
     $params->{field} = CF_WBS;
     $params->{id} = get_wbs_mapping($params->{tnerp_id});
-    delete_wbs_mapping($params->{tnerp_id});
     my $r = $self->SUPER::delete_value($params);
+    delete_wbs_mapping($params->{tnerp_id});
     return $r;
 }
 
