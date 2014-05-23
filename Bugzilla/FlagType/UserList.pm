51c183daf6b1676d331f79ecc649abb65befa6a4
Vitaliy Filippov <vitalif@yourcmc.ru>
2014-05-23 18:32:19 +0400

    Replace header

--- Bugzilla/FlagType/UserList.pm
+++ Bugzilla/FlagType/UserList.pm
@@ -1,16 +1,6 @@
-# -*- Mode: perl; indent-tabs-mode: nil -*-
-#
-# The contents of this file are subject to the Mozilla Public
-# License Version 1.1 (the "License"); you may not use this file
-# except in compliance with the License. You may obtain a copy of
-# the License at http://www.mozilla.org/MPL/
-#
-# Software distributed under the License is distributed on an "AS
-# IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
-# implied. See the License for the specific language governing
-# rights and limitations under the License.
-#
-# Contributor(s): Vitaliy Filippov <vitalif@mail.ru>
+# This module builds "custom" user lists for flag combo-box hints
+# License: Dual-license MPL 1.1+ or GPL 3.0+
+# Author(s): Vitaliy Filippov <vitalif@mail.ru>
 
 package Bugzilla::FlagType::UserList;
 
