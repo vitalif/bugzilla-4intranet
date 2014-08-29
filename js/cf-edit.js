3c200366824610136bbc1882b18728df686c7b0f
Vitaliy Filippov <vitalif@yourcmc.ru>
2014-08-29 18:47:50 +0400

    Reflow custom field edit page; hide "default value" for select fields being created.

--- js/cf-edit.js
+++ js/cf-edit.js
@@ -11,11 +11,16 @@ function onChangeType()
   {
     value_field.disabled = false;
     document.getElementById('value_field_row').style.display = '';
+    if (document.getElementById('action').value == 'new')
+    {
+      document.getElementById('default_value_row').style.display = 'none';
+    }
   }
   else
   {
     value_field.disabled = true;
     document.getElementById('value_field_row').style.display = 'none';
+    document.getElementById('default_value_row').style.display = '';
   }
   var rev_value_field = document.getElementById('bug_id_rev_value_field_id');
   if (type_field.value == constants.FIELD_TYPE_BUG_ID_REV)
@@ -30,16 +35,14 @@ function onChangeType()
     value_field.name = 'value_field_id';
     document.getElementById('bug_id_rev_row').style.display = 'none';
   }
-  document.getElementById('add_to_deps').style.display
-    = document.getElementById('add_to_deps_title').style.display
+  document.getElementById('add_to_deps_row').style.display
     = type_field.value == constants.FIELD_TYPE_BUG_ID ? '' : 'none';
   var u = document.getElementById('nullable');
   u.disabled = type_field.value != constants.FIELD_TYPE_SINGLE_SELECT;
   if (u.disabled)
     u.checked = false;
   u = type_field.value == constants.FIELD_TYPE_EXTURL;
-  document.getElementById('url_row').style.display
-    = u ? '' : 'none';
+  document.getElementById('url_row').style.display = u ? '' : 'none';
   onChangeNullable();
 }
 
