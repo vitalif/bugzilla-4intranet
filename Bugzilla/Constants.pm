85bdce23679e92cf42d748dabc25fb63b6425205
Vitaliy Filippov <vitalif@yourcmc.ru>
2014-10-17 19:42:11 +0400

    Bugzilla4Intranet notice and license for some templates

--- Bugzilla/Constants.pm
+++ Bugzilla/Constants.pm
@@ -196,7 +196,7 @@ use Cwd qw(abs_path);
 # CONSTANTS
 #
 # Bugzilla version
-use constant BUGZILLA_VERSION => "3.6.4";
+use constant BUGZILLA_VERSION => "2014.10-beta";
 
 # These are unique values that are unlikely to match a string or a number,
 # to be used in criteria for match() functions and other things. They start
